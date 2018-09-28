9f7b166db3b259e799022ca4f1faba3148d29f34
visitmathi <43663013+visitmathi@users.noreply.github.com>
2018-09-28 14:14:00 +0530

    Removed Jars from Lib Path and added in web-inf lib folder

--- src/main/java/com/detail/VehicleIdentifier.java
+++ src/main/java/com/detail/VehicleIdentifier.java
@@ -1,14 +1,13 @@
 package com.detail;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
 @Path("/Vehicle")
 public class VehicleIdentifier {
+	
 	static HashMap<String,Vehicle> vehicleMap = new HashMap<>();
 	
 	static {		
@@ -20,7 +19,11 @@ public class VehicleIdentifier {
 	@Path("/getVehicle/{vin}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getVehicle(@PathParam("vin") final String vin) {
+		if(vehicleMap.get(vin)!=null)
 		return Response.status(200).entity(vehicleMap.get(vin)).build();
+		else
+		return Response.status(204).entity("No data Available").build();
+				
 	}
 
 }
--- src/test/java/com/detail/VehicleIdentifierTest.java
+++ src/test/java/com/detail/VehicleIdentifierTest.java
@@ -1,16 +1,19 @@
 package com.detail;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import javax.ws.rs.core.Response;
 import org.junit.Test;
 
 public class VehicleIdentifierTest {
 	
-	@Test public void testGetVehicle() {
-		VehicleIdentifier vehicle=new VehicleIdentifier();
-		Response response = vehicle.getVehicle("MABCDEFGHIJKLMNO1");
+	@Test public void testValidVehicle() {
+		Response response = new VehicleIdentifier().getVehicle("MABCDEFGHIJKLMNO1");
 		assertEquals(response.getStatus(),200);	
+	}
+	
+	@Test public void testInvalidVehicle() {
+		Response response = new VehicleIdentifier().getVehicle("1");
+		assertEquals(response.getStatus(),204);	
 	}
 	
 
